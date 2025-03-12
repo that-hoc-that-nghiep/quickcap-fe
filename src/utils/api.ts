ac94e3288f093ada57cbce1cd6b8754ae8d60abb
LiusDev <quydx.work@gmail.com>
2025-03-12 21:42:11 +0700

    feat: add token for all instance request

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,28 @@
 import axios from 'axios'
 import { service } from './constant'
+import { useAuthStore } from '../stores/authStore'
 
 export const authInstance = axios.create({
     baseURL: service.auth
+})
+
+export const backendInstance = axios.create({
+    baseURL: service.backend
+})
+
+// Add request interceptors to include access token in headers
+authInstance.interceptors.request.use((config) => {
+    const accessToken = useAuthStore.getState().accessToken
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+})
+
+backendInstance.interceptors.request.use((config) => {
+    const accessToken = useAuthStore.getState().accessToken
+    if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
 })
